954962c87d97b2ad0190bd6419216c36cd9f0be7
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-06-03 22:43:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Main() {
+  return <div>Main</div>;
+}
+
+export default Main;

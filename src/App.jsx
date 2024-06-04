92cfe79d150071eaa95fb1837b95024a77bc0217
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-06-04 21:35:14 +0900

    note追加機能途中まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function onAddNote() {
+    const newNote = {
+      id: 1,
+      title: "新ノート",
+      content: "新ノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+  }
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   );

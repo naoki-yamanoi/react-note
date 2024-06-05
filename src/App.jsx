f849291c178ce147a7276c270a8c00ae70661bbc
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-06-05 20:18:39 +0900

    note削除機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import { useState } from "react";
+import uuid from "react-uuid";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function onAddNote() {
     const newNote = {
-      id: 1,
+      id: uuid(),
       title: "新ノート",
       content: "新ノートの内容",
       modDate: Date.now(),
@@ -16,9 +17,20 @@ function App() {
     setNotes([...notes, newNote]);
   }
 
+  const onDeleteNote = (id) => {
+    const result = notes.filter((note) => {
+      return note.id !== id;
+    });
+    setNotes(result);
+  };
+
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} notes={notes} />
+      <Sidebar
+        onAddNote={onAddNote}
+        notes={notes}
+        onDeleteNote={onDeleteNote}
+      />
       <Main />
     </div>
   );
